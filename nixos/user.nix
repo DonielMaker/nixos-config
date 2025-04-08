dce819a35208346ef5a0fe09e44f0e3ff0912c7a
donielmaker <daniel.schmidt0204@gmail.com>
2025-04-09 00:02:54 +0200

    added shell compat

--- nixos/user.nix
+++ nixos/user.nix
@@ -1,11 +1,11 @@
-{pkgs, username, ...}:
+{pkgs, username, shell, ...}:
 
 {
     users.users.${username} = {
         isNormalUser = true;
         description = username;
         extraGroups = ["networkmanager" "wheel" "input" "audio"];
-        shell = pkgs.zsh;
+        shell = pkgs.${shell};
         initialPassword = "Changeme";
         openssh.authorizedKeys.keys = [];
     };
