3c3ca90edbffe59259d42ac2fe5afd82395b30c0
donielmaker <daniel.schmidt0204@gmail.com>
2025-04-13 12:45:57 +0200

    fixed firefox

--- hm/firefox.nix
+++ hm/firefox.nix
@@ -1,27 +1,34 @@
 { pkgs, pkgs-firefox, username, ...}: {
+    programs.firefox.enable = true;
+
     programs.firefox = {
-        enable = true;
 
         profiles.${username} = {
-            search.engines."Nix Packages" = {
-                urls = [{
-                    template = "https://search.nixos.org/packages";
-                    params = [
-                        {name = "type"; value = "packages";}
-                        {name = "query"; value = "{searchTerms}";}
-                    ];
-                }];
+            search = {
+                force = true;
+                default = "DuckDuckGo";
+                privateDefault = "DuckDuckGo";
+                engines = {
+                    "Nix Packages" = {
+                        urls = [{
+                            template = "https://search.nixos.org/packages";
+                            params = [
+                            {name = "type"; value = "packages";}
+                            {name = "query"; value = "{searchTerms}";}
+                            ];
+                        }];
 
-                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                definedAliases = [ "@np" ];
+                        icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                        definedAliases = [ "@np" ];
+                    };
+                };
             };
-            search.force = true;
 
-            extensions = with pkgs-firefox;[
-                bitwarden
-                ublock-origin
-                sponsorblock
-            ];
+# extensions = with pkgs-firefox;[
+#     bitwarden
+#     ublock-origin
+#     sponsorblock
+# ];
         };
     };
 }
