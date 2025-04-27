30982c8fe48fab246368ac9c3c0924967883f2fe
donielmaker <daniel.schmidt0204@gmail.com>
2025-04-27 11:30:28 +0200

    added coolercontrol pkgs

--- nixos/coolercontrol.nix
+++ nixos/coolercontrol.nix
@@ -1,3 +1,7 @@
+{pkgs-stable, ...}:
+
 {
     programs.coolercontrol.enable = true;
+
+    environment.systemPackages = with pkgs-stable.coolercontrol; [ coolercontrold coolercontrol-gui coolercontrol-ui-data coolercontrol-liqctld ];
 }
