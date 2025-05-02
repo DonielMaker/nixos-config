e591fd0080a8748c46b897a3b694abfed2c8e873
donielmaker <daniel.schmidt0204@gmail.com>
2025-05-02 15:48:46 +0200

    formatting

--- flake.nix
+++ flake.nix
@@ -34,10 +34,9 @@
         nixosConfigurations.galaxia = mkNixos ./hosts/galaxia;
         nixosConfigurations.zenith = mkNixos ./hosts/zenith;
         nixosConfigurations."server" = mkNixos ./hosts/server;
+
         # DEPRECATED for now
         # nixosConfigurations.wsl = mkNixos ./hosts/wsl;
-
-        # packages.${system}.rofi-spotlight = pkgs.callPackage ./nixos/rofi-spotlight.nix {};
 
         devShells.${system}.rust = (import ./testing/rust.nix {inherit pkgs;});
     };
