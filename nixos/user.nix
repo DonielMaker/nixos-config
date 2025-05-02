f92bdc986e07a30f6db29fbc7ba5487548b98279
donielmaker <daniel.schmidt0204@gmail.com>
2025-05-02 11:33:29 +0200

    added new ssh key

--- nixos/user.nix
+++ nixos/user.nix
@@ -7,6 +7,8 @@
         extraGroups = ["networkmanager" "wheel" "input" "audio"];
         shell = pkgs.${shell};
         initialPassword = "Changeme";
-        openssh.authorizedKeys.keys = [];
+        openssh.authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwosE68FthKwXs1WhPnY3YqbkVPT52V30X489epRsJQ donielmaker@zenith"
+        ];
     };
 }
