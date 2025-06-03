e4c26dd3cb4f91dc0e072508f5f85c8b4aef9ec9
donielmaker <daniel.schmidt0204@gmail.com>
2025-06-03 11:00:53 +0200

    (stable?) traefik config

--- nixos/traefik.nix
+++ nixos/traefik.nix
@@ -1,62 +1,73 @@
-{...}:
+{config, ...}:
 
 {
-   networking.firewall.allowedTCPPorts = [80 443];
+    networking.firewall.allowedTCPPorts = [ 22 80 443 ];
 
     services.traefik.enable = true;
-    #services.traefik.staticConfigFile = ./traefik.toml;
     services.traefik.staticConfigOptions = {
         api = {
             dashboard = true;
-            debug = false;
+        };
+
+        log = {
+            level = "WARN";
+            filepath = "${config.services.traefik.dataDir}/traefik.log";
+            format = "json";
         };
 
         entryPoints = {
-            #http = {
-            #    address = 80;
-            #    http = {
-            #        redirections.entryPoint.to = "https";
-            #        redirections.entryPoint.scheme = "https";
-            #    };
-            #};
+            http = {
+               address = ":80";
+               # http = {
+               #     redirections.entryPoint.to = "https";
+               #     redirections.entryPoint.scheme = "https";
+               # };
+            };
 
-            https.address = ":443";
+            https = {
+                address = ":443";
+                # http.tls.certResolver = "cloudflare";
+            };
+        };
 
-            #serversTransport.insecureSkipVerify = true;
+        serversTransport.insecureSkipVerify = true;
 
-            #certificatesResolvers."cloudflare".acme = {
-            #    email = "daniel.schmidt0204@gmail.com";
-            #    storage = "acme.json";
-            #    #caServer = "https://acme-v02.api.letsencrypt.org/directory"; # prod
-            #    caServer = "https://acme-staging-v02.api.letsencrypt.org/directory"; # staging
-            #        dnsChallenge = {
-            #            provider = "cloudflare";
-            #            resolvers = [
-            #                "1.1.1.1:53"
-            #                    "1.0.0.1:53"
-            #            ];
-            #        };
-            #};
+        certificatesResolvers.cloudflare.acme = {
+           email = "daniel.schmidt0204@gmail.com";
+           storage = "${config.services.traefik.dataDir}/acme.json";
+           #caServer = "https://acme-v02.api.letsencrypt.org/directory"; # prod
+           caServer = "https://acme-staging-v02.api.letsencrypt.org/directory"; # staging
+           dnsChallenge = {
+               provider = "cloudflare";
+               resolvers = [
+                   "1.1.1.1:53"
+                   "1.0.0.1:53"
+               ];
+           };
         };
     };
-    services.traefik.environmentFiles = [
-	/home/donielmaker/.config/nixos-config/nixos/.env
-    ];
+
     services.traefik.dynamicConfigOptions = {
         http = {
             routers = {
                 traefik = {
-                    entrypoints = "https";
                     rule = "Host(`traefik.test.thematt.net`)";
                     service = "api@internal";
                 };
+                sf = {
+                    rule = "Host(`seafile.test.thematt.net`)";
+                    service = "sf";
+                };
             };
-
-            services
+            services = {
+                sf.loadBalancer.servers = [{url = "http://10.10.12.14:8000";}];
+            };
         };
+    };      
+}
 
       #   # tls config
-      # - "traefik.http.routers.traefik.tls=true"
+      # - "traefik.http.routes.traefik.tls=true"
       # - "traefik.http.routers.traefik.tls.certresolver=cloudflare"
       #
       # - "traefik.http.routers.traefik.tls.domains[0].main=thematt.net"
@@ -67,5 +78,3 @@
       #
       # - "traefik.http.routers.traefik.tls.domains[2].main=lastprism.thematt.net"
       # - "traefik.http.routers.traefik.tls.domains[2].sans=*.lastprism.thematt.net" 
-    };      
-}
