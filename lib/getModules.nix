2a068487b6b00365dfcd9af9bfbafe51cf50365e
donielmaker <daniel.schmidt0204@gmail.com>
2025-07-13 22:13:53 +0200

    CHANGE: new getModules

--- lib/getModules.nix
+++ lib/getModules.nix
@@ -1,8 +1,9 @@
-{lib, ...}:
+{lib}:
 
 let
-    filesIn = dir: builtins.attrNames (builtins.readDir dir);
-    namesIn = dir: map(file: lib.removeSuffix ".nix" file) (filesIn dir);
-    pathsIn = dir: map(file: dir + "/${file}") (filesIn dir);
-    zipPairs = dir: map(attr: {name = attr.fst; value = attr.snd;}) (lib.zipLists (namesIn dir) (pathsIn dir));
+    getFile = dir: lib.filesystem.listFilesRecursive dir;
+    getNixFile = dir: lib.filter (file: lib.hasSuffix ".nix" file) (getFile dir);
+    getBaseFile = dir: lib.map (file: baseNameOf file) (getNixFile dir);
+    getModuleName = dir: lib.map (file: lib.removeSuffix ".nix" file) (getBaseFile dir);
+    zipPairs = dir: map(attr: {name = attr.fst; value = attr.snd;}) (lib.zipLists (getModuleName dir) (getNixFile dir));
 in dir: builtins.listToAttrs (zipPairs dir)
