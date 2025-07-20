dcf56a70a2bf89296f952d7009c546aaeae00840
donielmaker <daniel.schmidt0204@gmail.com>
2025-07-20 20:59:18 +0200

    formatting

--- hosts/zenith/secrets/secrets.nix
+++ hosts/zenith/secrets/secrets.nix
@@ -1,12 +1,12 @@
 let
-    user1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwosE68FthKwXs1WhPnY3YqbkVPT52V30X489epRsJQ donielmaker@zenith";
+    donielmaker = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwosE68FthKwXs1WhPnY3YqbkVPT52V30X489epRsJQ donielmaker@zenith";
 
-    system1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE9OsEE+anE42quYmi3ewEsxA/jiopIzdjdAaXa05t/m";
+    zenith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE9OsEE+anE42quYmi3ewEsxA/jiopIzdjdAaXa05t/m";
 in
 {
-    "secret1.age".publicKeys = [ user1 system1 ];
-    "secret2.age".publicKeys = [ user1 system1 ];
+    "secret1.age".publicKeys = [ donielmaker zenith ];
+    "secret2.age".publicKeys = [ donielmaker zenith ];
 
-    "wireguard-shrKey.age".publicKeys = [ user1 system1 ];
-    "wireguard-priKey.age".publicKeys = [ user1 system1 ];
+    "wireguard-shrKey.age".publicKeys = [ donielmaker zenith ];
+    "wireguard-priKey.age".publicKeys = [ donielmaker zenith ];
 }
