f85efbc3b4523e0521acd267f5618bbdff1feecf
donielmaker <daniel.schmidt0204@gmail.com>
2025-07-21 14:16:47 +0200

    added secrets; and auth suite; fixed sessionEncryptionKeyFile.age to sessionEncryptionKey.age

--- hosts/lastprism/secrets/secrets.nix
+++ hosts/lastprism/secrets/secrets.nix
@@ -2,11 +2,12 @@ let
     donielmaker-zenith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwosE68FthKwXs1WhPnY3YqbkVPT52V30X489epRsJQ donielmaker@zenith";
 
     zenith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE9OsEE+anE42quYmi3ewEsxA/jiopIzdjdAaXa05t/m";
+    lastprism = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII2FlssgN97CKHkigZ/C6bGLTEkB+IbPZQHK/0ybIBF6";
 in
 {
-    "jwtSecret.age".publicKeys = [donielmaker-zenith zenith];
-    "storageEncryptionKeyFile.age".publicKeys = [donielmaker-zenith zenith];
-    "sessionSecret.age".publicKeys = [donielmaker-zenith zenith];
-    "autheliaLldapPassword.age".publicKeys = [donielmaker-zenith zenith];
-    "cloudflare.age".publicKeys = [donielmaker-zenith zenith];
+    "jwtSecret.age".publicKeys = [donielmaker-zenith zenith lastprism];
+    "storageEncryptionKey.age".publicKeys = [donielmaker-zenith zenith lastprism];
+    "sessionSecret.age".publicKeys = [donielmaker-zenith zenith lastprism];
+    "autheliaLldapPassword.age".publicKeys = [donielmaker-zenith zenith lastprism];
+    "cloudflare.age".publicKeys = [donielmaker-zenith zenith lastprism];
 }
