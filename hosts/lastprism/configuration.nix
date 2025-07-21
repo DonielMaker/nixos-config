c8820b3899ec05e2a13e16a3a1d1f756ed7aed78
donielmaker <daniel.schmidt0204@gmail.com>
2025-07-21 14:21:32 +0200

    added some more remote-build settings

--- hosts/lastprism/configuration.nix
+++ hosts/lastprism/configuration.nix
@@ -14,19 +14,28 @@
         user
 
         # TODO: add the lastprism system to the secrets.nix
-        caddy
-        authelia
-        lldap
+        # caddy
+        # authelia
+        # lldap
     ];
 
     age.secrets = myLib.getSecrets ./secrets;
 
+    security.sudo.execWheelOnly  =  true;
+
     services.openssh.enable = true;
+    services.openssh.settings = {
+        PasswordAuthentication = false;
+        PermitRootLogin = "no";
+    };
+
+    nix.settings.trusted-users = [ "donielmaker" ];
 
     environment.systemPackages = with pkgs; [
         inputs.ragenix.packages.${system}.default
 
         vim
+        git
     ];
 
     system.stateVersion = "25.11"; # Just don't
