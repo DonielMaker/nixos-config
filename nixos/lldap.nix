017554920753f802553829498353d2d69e53a9b5
donielmaker <daniel.schmidt0204@gmail.com>
2025-07-21 22:45:32 +0200

    DONE: auth suite setup now to integrate

--- nixos/lldap.nix
+++ nixos/lldap.nix
@@ -3,5 +3,7 @@
 {
     services.lldap.enable = true;
     services.lldap.settings.ldap_base_dn = "dc=thematt,dc=net";
+    # User does no longer exist
+    # services.lldap.settings.ldap_user_pass = "blablabla";
     networking.firewall.allowedTCPPorts = [ 17170 3890 ];
 }
