71b10042d7dd89b894195b7f9c43dc847b6164be
donielmaker <daniel.schmidt0204@gmail.com>
2025-07-22 03:17:43 +0200

    removed bad hosts

--- hosts/nfs-server/settings.nix
+++
@@ -1,9 +0,0 @@
-rec {
-    hostname = "nfs-server";
-    system = "x86_64-linux";
-    username = "donielmaker";
-    dotfiles = "/home/${username}/.config";
-    timezone = "Europe/Berlin";
-    shell = "bash";
-    drive = "/dev/sdb";
-}
