56a0f57ae4ef31173e56eea53ef6ce814fe6d987
donielmaker <daniel.schmidt0204@gmail.com>
2025-07-29 12:16:35 +0200

    added search domain

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -2,6 +2,9 @@
 
 {
     networking.networkmanager.enable = true;
+    networking.search = [
+        "thematt.net"
+    ];
     networking.hostName = hostname;
     networking.nameservers = [
         "10.10.12.2"
