97a5cd4c7c90c0021cc74d062b88b077b66b5fe0
donielmaker <daniel.schmidt0204@gmail.com>
2025-07-31 23:16:35 +0200

    moved all traefik/authentik hosts to caddy/authelia :)

--- nixos/caddy.nix
+++ nixos/caddy.nix
@@ -4,7 +4,7 @@
     networking.firewall.allowedTCPPorts = [ 80 443 8000];
     services.caddy.enable = true;
     services.caddy.extraConfig = ''
-        *.thematt.net {
+        *.thematt.net, *.vilethorn.thematt.net, *.lastprism.thematt.net {
             tls /var/lib/acme/thematt.net/cert.pem /var/lib/acme/thematt.net/key.pem {
                 protocols tls1.3
             }
@@ -14,46 +14,41 @@
                 reverse_proxy 10.10.12.3:9091
             }
 
-            @api-lldap host api.lldap.thematt.net
-            handle @api-lldap {
-                reverse_proxy 10.10.12.3:3890
-            }
-
             @lldap host lldap.thematt.net
             handle @lldap {
                 reverse_proxy 10.10.12.3:17170
             }
 
-            @whoami host whoami.test.thematt.net
-            handle @whoami {
+            @wireguard host wireguard.thematt.net 
+            handle @wireguard {
                 forward_auth 10.10.12.3:9091 {
                     uri /api/authz/forward-auth
                     copy_headers Remote-User Remote-Groups Remote-Name Remote-Email
                 }
-                reverse_proxy 10.10.12.3:8000
+                reverse_proxy 10.10.12.2:51821
             }
 
-            @clst-1 host clst-1.thematt.net 
-            handle @clst-1 {
-                reverse_proxy 10.10.12.60:8006 {
+            @technitium host technitium.thematt.net 
+            handle @technitium {
+                forward_auth 10.10.12.3:9091 {
+                    uri /api/authz/forward-auth
+                    copy_headers Remote-User Remote-Groups Remote-Name Remote-Email
+                }
+                reverse_proxy 10.10.12.2:5380
+            }
+
+            @proxmox-vilethorn host proxmox.vilethorn.thematt.net 
+            handle @proxmox-vilethorn {
+                reverse_proxy 10.10.12.11:8006 {
                     transport http {
                         tls_insecure_skip_verify
                     }
                 }
             }
 
-            @clst-2 host clst-2.thematt.net 
-            handle @clst-2 {
-                reverse_proxy 10.10.12.61:8006 {
-                    transport http {
-                        tls_insecure_skip_verify
-                    }
-                }
-            }
-
-            @clst-3 host clst-3.thematt.net 
-            handle @clst-3 {
-                reverse_proxy 10.10.12.62:8006 {
+            @proxmox-lastprism host proxmox.lastprism.thematt.net 
+            handle @proxmox-lastprism {
+                reverse_proxy 10.10.12.12:8006 {
                     transport http {
                         tls_insecure_skip_verify
                     }
