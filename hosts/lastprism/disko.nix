f363ad37a7f7a05acdf620e62045d0df6e385c69
donielmaker <daniel.schmidt0204@gmail.com>
2025-08-09 10:01:51 +0200

    new drive layout; added opencloud

--- hosts/lastprism/disko.nix
+++ hosts/lastprism/disko.nix
@@ -1,28 +1,46 @@
-{drive ? throw "please select a device", ...}:
-
 {
-    disko.devices.disk.main = {
-        device = drive;
-        type = "disk";
-        content = {
-            type = "gpt";
-            partitions = {
-                ESP = {
-                    type = "EF00";
-                    size = "500M";
-                    content = {
-                        type = "filesystem";
-                        format = "vfat";
-                        mountpoint = "/boot";
-                        mountOptions = [ "umask=0077" ];
+    disko.devices.disk = {
+        main = {
+            device = "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0";
+            type = "disk";
+            content = {
+                type = "gpt";
+                partitions = {
+                    ESP = {
+                        type = "EF00";
+                        size = "500M";
+                        content = {
+                            type = "filesystem";
+                            format = "vfat";
+                            mountpoint = "/boot";
+                            mountOptions = [ "umask=0077" ];
+                        };
+                    };
+                    root = {
+                        size = "100%";
+                        content = {
+                            type = "filesystem";
+                            format = "ext4";
+                            mountpoint = "/";
+                        };
                     };
                 };
-                root = {
-                    size = "100%";
-                    content = {
-                        type = "filesystem";
-                        format = "ext4";
-                        mountpoint = "/";
+            };
+        };
+
+        disk1 = {
+            device = "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi1";
+            type = "disk";
+            content = {
+                type = "gpt";
+                partitions = {
+                    opencloud = {
+                        size = "100%";
+                        content = {
+                            type = "filesystem";
+                            format = "ext4";
+                            mountpoint = "/var/lib/opencloud";
+                        };
                     };
                 };
             };
