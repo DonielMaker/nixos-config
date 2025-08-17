0c76a3cdb6e474195762c8b9e72e413e5b5d1102
donielmaker <daniel.schmidt0204@gmail.com>
2025-08-17 13:29:06 +0200

    added galaxia key to users

--- nixos/user.nix
+++ nixos/user.nix
@@ -9,6 +9,7 @@
         initialPassword = "Changeme";
         openssh.authorizedKeys.keys = [
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwosE68FthKwXs1WhPnY3YqbkVPT52V30X489epRsJQ donielmaker@zenith"
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMGCA4si0+PqWQn/PRtfOeuCIK3ISIvMKfvQhciwPQvh donielmaker@galaxia"
         ];
     };
 }
