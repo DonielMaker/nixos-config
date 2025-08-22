77659d10eade2116998023feccc7fa00e565854e
donielmaker <daniel.schmidt0204@gmail.com>
2025-08-22 22:32:02 +0200

    removed wg config

--- hosts/zenith/configuration.nix
+++ hosts/zenith/configuration.nix
@@ -39,26 +39,6 @@
     programs.nautilus-open-any-terminal.enable = true;
     programs.nautilus-open-any-terminal.terminal = "alacritty";
 
-    networking.search = [ "thematt.net" ];
-
-    # programs.localsend.enable = true;
-
-    networking.wg-quick.interfaces.wg0 = {
-        address = [ "10.10.14.7/24" ];
-        dns = [ "1.1.1.1" ];
-        peers = [
-        {
-            allowedIPs = [
-                "10.10.14.0/24"
-                "10.10.12.0/24"
-            ];
-            endpoint = "public.ipv64.de:51820";
-            publicKey = "Ltmlc2mcJuKprhi5l6rU2hwMqejwQIQ/GFZB+sEckCQ=";
-            presharedKeyFile = config.age.secrets.wireguard-shrKey.path;
-        }
-        ];
-        privateKeyFile = config.age.secrets.wireguard-priKey.path;
-    };
 
     services.lact.enable = true;
 
