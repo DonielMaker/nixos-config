f76317f98fe37d9e7e867affebe76b885494620b
donielmaker <daniel.schmidt0204@gmail.com>
2025-09-05 15:42:10 +0200

    More Work!!!

---
+++ TODO.md
@@ -0,0 +1,3 @@
+# TODO
+- Instead of using a settings.nix in all of the hosts we would like the dynamic configuration to be defined in either 
+configuration.nix or in home.nix. This way we don't have a file that doesn't follow the nixos module standard (Right now it is just a .nix file).
--- hosts/galaxia/home.nix
+++ hosts/galaxia/home.nix
@@ -18,6 +18,35 @@
         alacritty
     ];
 
+    # FIX: there's still a problem with the duplication between nixos and hm!!!
+
+    # This is configurable via: networking.hostName = hostname;
+    #     hostname = "galaxia";
+
+    # FIX: This technically already is configured ???
+    #     system = "x86_64-linux";
+
+    # This is a lot harder as it would mean moving the entire users.users.<name>
+    # config to the specific hosts
+    #     username = "donielmaker";
+
+    # programs.git.userEmail
+    #     mail = "daniel.schmidt0204@gmail.com";
+
+    # This should be done via $XDG_CONFIG_HOME
+    #     dotfiles = "/home/${username}/.config";
+
+    # Both are hyprland configs
+    # WARN: kb_layout is required by both nixos and hm
+    #     kb_layout = "de";
+    #     monitor = ", 1920x1080@60hz, auto, 1";
+
+    # time.timeZone
+    #     timezone = "Europe/Berlin";
+
+    # users.users.<name>.shell
+    #     shell = "zsh";
+
     home = {
         inherit username;
         homeDirectory = lib.mkForce "/home/${username}";
