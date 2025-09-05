792f2ae610b023bfb1b23126d4fd8bf53885bec2
donielmaker <daniel.schmidt0204@gmail.com>
2025-09-05 11:17:01 +0200

    new working wireguar config

--- hosts/galaxia/configuration.nix
+++ hosts/galaxia/configuration.nix
@@ -30,37 +30,40 @@
     programs.nautilus-open-any-terminal.enable = true;
     programs.nautilus-open-any-terminal.terminal = "alacritty";
 
-    # networking.wg-quick.interfaces.wg0 = {
-    #     address = [ "10.10.14.6/24" ];
-    #     dns = [ "1.1.1.1" ];
-    #     peers = [
-    #         {
-    #             allowedIPs = [
-    #                 "10.10.14.0/24"
-    #                 "10.10.12.0/24"
-    #             ];
-    #             endpoint = "87.186.6.152:51820";
-    #             publicKey = "Ltmlc2mcJuKprhi5l6rU2hwMqejwQIQ/GFZB+sEckCQ=";
-    #             presharedKeyFile = config.age.secrets.wireguard-shrKey.path;
-    #         }
-    #     ];
-    #     privateKeyFile = config.age.secrets.wireguard-priKey.path;
-    # };
+    networking.wg-quick.interfaces.wg0 = {
+        address = [ "10.8.0.3/24" ];
+        dns = [ "1.1.1.1" ];
+        peers = [
+            {
+                allowedIPs = [
+                    "10.8.0.0/24"
+                    "10.10.12.0/24"
+                ];
+                endpoint = "public.ipv64.de:51820";
+                publicKey = "hy12YOG6MaWwA3JeqGpDzt0XXSg/qsErxIRntlVrzHU=";
+                presharedKeyFile = config.age.secrets.wireguard-shrKey.path;
+            }
+        ];
+        privateKeyFile = config.age.secrets.wireguard-priKey.path;
+    };
 
-    #age.secrets = {
-    #    wireguard-priKey.file = ./secrets/wireguard-priKey.age;
-    #    wireguard-shrKey.file = ./secrets/wireguard-shrKey.age;
-    #};
+    age.secrets = {
+       wireguard-priKey.file = ./secrets/wireguard-priKey.age;
+       wireguard-shrKey.file = ./secrets/wireguard-shrKey.age;
+    };
 
     programs.localsend.enable = true;
 
     services.openssh.enable = true;
 
     environment.systemPackages = with pkgs; [
+        supersonic-wayland
+        opencloud-desktop
         inputs.quickshell.packages.${system}.quickshell
         inputs.ragenix.packages.${system}.default
         ferrishot
         signal-desktop
+        orca-slicer
         # Programs
         nautilus
         hyprpicker
