1b102352654625e0e9419d80acc94ef5af1814aa
donielmaker <daniel.schmidt0204@gmail.com>
2025-09-06 10:51:22 +0200

    move jwks into secret

--- nixos/authelia.nix
+++ nixos/authelia.nix
@@ -18,6 +18,7 @@
         };
         environmentVariables = {
             "AUTHELIA_AUTHENTICATION_BACKEND_LDAP_PASSWORD_FILE" = config.age.secrets.autheliaLldapPassword.path;
+            "AUTHELIA_OIDC_JWKS_MAIN_KEY" = config.age.secrets.autheliaJwksKey.path;
         };
         settings = {
             theme = "dark";
@@ -86,39 +87,9 @@
             };
             identity_providers = {
                 oidc = {
-# hmac_secret = "this_is_a_secret_abc123abc123abc";
                     jwks = [
                         {
-                            # key = ''{{ secret "${config.age.secrets."private.pem".path}" | minindent 10 "|" msquote }}'';
-                            key = 
-''-----BEGIN PRIVATE KEY-----
-MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDu0gDzjRSZsico
-lZVsR7FJhhA3ImeL+sg82j/fXYvN6oEk26lgIipD+r/zIgNoBYaaDpNL94RzZ3JN
-W5FuMq15oFEC1gTS/MekfMuD+gG0t5pCuCwt3cC11dZP+b8C/mhdRk+tvMElbVml
-zY5B7QcwtItrxwI8qTfINDifgqeRdCchFUcfsahJbg72aifs2jua+HHk5ZqVajId
-Wa26Dq2aJqAom2mWp7fFjB+GThSFSOa1+oYHtu7IlP+XpCyppO1961zEsoD5n732
-whreEFsV2r/SlPOY81DJBD57KBvgu78H2c46NSRkyKh6QJL88u6lFxbjb4WjYfxg
-pu9Lj+/rAgMBAAECggEALqiQzwSNNcn8pI00+Ea5eGfc2yi5mjuQy3LIb9dS7RQX
-zx+rY8DJCScAQXwvti7+MTMPzBO7qOBZEIwHScRU6CJOMYOmxaHlT1miWVKK6sfE
-N6zL1NoyQFRa3p0omGkj8rU98++gvLM0hJQ4auDoOrfLVW84HKni4BZzyZY5Ls1l
-/Gj++X3ja9sXN6NTD4EnbgdqOve/VQmSoqGf7RVSinbuWPAPqhodkw/IHIEdiKMP
-Cz0FGeoTAOiE340ZBjf4lyYJOf0FZyE15r3MduiTwzyNENMxMxaZKcB3i4c4GqoD
-MF76y7zKG91WfSyVwFwZCEQHlkuwvOwE9/TWeJ5cYQKBgQD/i/4Al8Ag3w0ZIo/e
-40JzE0ez0V+dmJH9wCF823z8Srb7s9P+br5XnlMfIt79tOTN7mRyPvc9hQUfJAQp
-LPFdoH3eiH7ueMHf72BQzgKCDRAfFATft3qj+k2YnBgxa3WFVzZE3Im40EiCrXOe
-Wj5XGREi6jYKWqbEnIFi/Et3tQKBgQDvPmsaA+Mx96T1Ihjuh3gicDNqnVxa8U9f
-tN3tjmjAv+53btALOP2doWW5XWlKSGvw04BROe4PQUCV/lef7pDgatnaC8WJ8WtS
-jaqAoarx/YI+lmkCZo3IjULl7nEsPWjRRmN+Rme5dnDnxObPG8ApGB5Es6klYPLX
-t3l5Kp9NHwKBgHhG7bBdvGI8Wa/g4pJnqJj9FByAiV5ltyEV0y0vKW6dw+5IP5lb
-il8296yJ/yaug5qhf8l99WSRPU3g58xxiNEmftfEqumXELlTs6OUsv1/JH1cMMcT
-VrfjUa5omcJ474FpAPP8UR4Zemd3OBnqI7le5P/n+LN6409a0lNCqEqZAoGBAIdr
-ZWRMXwWhj5XRvxsFQ1L6/5+Rgv1XKf6aeiRFP7Ya7fhcmRZBJA1dlh7eaQLLC449
-qZxQlhe8b7p3RVIZz5e346gwkZgLNxx7Y897sy9Wt6qFqNrbAdZLX1qXJShNek86
-8IZBg+TOzgniwmLo6lRYeomYyF2ICu1fxTp4jcMxAoGBAJp3yArOO8YCLxAcfni4
-EwR0v9G4MDqIh+IbjCX4H0Tt+4doJABeDGXn6xxcpwKLBgCrSSkB3jt+IfcKeuww
-NK9iY/qavsvwYFA84O0rEnb1uzN/RES/bE+3CVucmobhBFjZ5qZMHbVC1tZNSsfI
-OOgp6OSXLu5imu8gkIrqxA+e
------END PRIVATE KEY-----'';
+                            key = "\${AUTHELIA_OIDC_JWKS_MAIN_KEY}";
                         }
                     ];
                     lifespans = {
@@ -133,9 +104,9 @@ OOgp6OSXLu5imu8gkIrqxA+e
                     cors = {
                         endpoints= [
                             "authorization"
-                                "token"
-                                "revocation"
-                                "introspection"
+                            "token"
+                            "revocation"
+                            "introspection"
                         ];
                         allowed_origins= [ "https://*.thematt.net" ];
                     };
