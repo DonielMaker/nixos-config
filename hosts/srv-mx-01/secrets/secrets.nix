d5e1d6415c76b6c08ad8101995ffb378cab3ea90
donielmaker <daniel.schmidt0204@gmail.com>
2025-09-24 17:18:02 +0200

    new user

--- hosts/srv-mx-01/secrets/secrets.nix
+++ hosts/srv-mx-01/secrets/secrets.nix
@@ -1,16 +1,16 @@
 let
     donielmaker-zenith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwosE68FthKwXs1WhPnY3YqbkVPT52V30X489epRsJQ donielmaker@zenith";
-    donielmaker-lastprism-max = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMrlNi2ZBhJmeNEiVLg6fqIywfg3nZnRZ99xqVYWZbZN donielmaker@lastprism-max";
-    users = [donielmaker-zenith donielmaker-lastprism-max];
+    solut-srv-mx-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMrlNi2ZBhJmeNEiVLg6fqIywfg3nZnRZ99xqVYWZbZN donielmaker@lastprism-max";
+    anton-spc-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDwnQCbrIxOgzRpoEYyb/SaoDhM5i93wWJ8biH/f8ygT deuyanon@SPC-01";
+    users = [donielmaker-zenith solut-srv-mx-01 anton-spc-01];
 
     zenith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE9OsEE+anE42quYmi3ewEsxA/jiopIzdjdAaXa05t/m";
-    lastprism-max = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBmS6gxfWFbVP5qZPxAgcGjKbZHYqQcfBODSiGjoY3M0";
-    systems = [zenith lastprism-max];
+    srv-mx-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBmS6gxfWFbVP5qZPxAgcGjKbZHYqQcfBODSiGjoY3M0";
+    spc-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFSM5bfdGl9OUuKchV23PVogIf0ZGvH3XeOwtzuuDfep";
+    systems = [zenith srv-mx-01 spc-01];
+
+     
 in
 {
-    # "jwtSecret.age".publicKeys = [donielmaker-zenith donielmaker-lastprism zenith lastprism];
-    # "storageEncryptionKey.age".publicKeys = [donielmaker-zenith donielmaker-lastprism zenith lastprism];
-    # "sessionSecret.age".publicKeys = [donielmaker-zenith donielmaker-lastprism zenith lastprism];
-    # "autheliaLldapPassword.age".publicKeys = [donielmaker-zenith donielmaker-lastprism zenith lastprism];
-    "ipv64DnsApiToken.age".publicKeys = [donielmaker-zenith donielmaker-lastprism-max zenith lastprism-max];
+    "ipv64DnsApiToken.age".publicKeys = users ++ systems;
 }
