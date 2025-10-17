ed6ef621f660edad1bd93117feaceb24ec758040
donielmaker <daniel.schmidt0204@gmail.com>
2025-10-17 23:52:25 +0200

    added alloy as a service

--- hosts/lastprism/configuration.nix
+++ hosts/lastprism/configuration.nix
@@ -15,6 +15,7 @@
         user
         # docker
         openssh
+        alloy
 
         lldap
 
@@ -125,12 +126,12 @@
     };
 
     # Tika: Ocr?
-    services.tika.enable = true;
-    services.tika = {
-        listenAddress = "0.0.0.0";
-        openFirewall = true;
-        enableOcr = true;
-    };
+    # services.tika.enable = true;
+    # services.tika = {
+    #     listenAddress = "0.0.0.0";
+    #     openFirewall = true;
+    #     enableOcr = true;
+    # };
 
     # Homepage: a Dashboard for all your needs
     services.homepage-dashboard.enable = true;
@@ -246,55 +247,6 @@
         webExternalUrl = "https://prometheus.thematt.net";
         port = 9090;
     };
-
-    services.alloy.enable = true;
-    services.alloy.extraFlags = [ "--server.http.listen-addr=0.0.0.0:12345" ];
-    environment.etc."alloy/config.alloy".text = ''
-        prometheus.exporter.unix "metrics" {
-            disable_collectors = ["ipvs", "btrfs", "infiniband", "xfs", "zfs"]
-            enable_collectors = ["meminfo"]
-
-            filesystem {
-                fs_types_exclude     = "^(autofs|binfmt_misc|bpf|cgroup2?|configfs|debugfs|devpts|devtmpfs|tmpfs|fusectl|hugetlbfs|iso9660|mqueue|nsfs|overlay|proc|procfs|pstore|rpc_pipefs|securityfs|selinuxfs|squashfs|sysfs|tracefs)$"
-                    mount_points_exclude = "^/(dev|proc|run/credentials/.+|sys|var/lib/docker/.+)($|/)"
-                    mount_timeout        = "5s"
-            }
-
-            netclass {
-                ignored_devices = "^(veth.*|cali.*|[a-f0-9]{15})$"
-            }
-
-            netdev {
-                device_exclude = "^(veth.*|cali.*|[a-f0-9]{15})$"
-            }
-        }
-
-        discovery.relabel "metrics" {
-            targets = prometheus.exporter.unix.metrics.targets
-
-            rule {
-                target_label = "instance"
-                    replacement  = constants.hostname
-            }
-
-            rule {
-                target_label = "job"
-                    replacement = string.format("%s-metrics", constants.hostname)
-            }
-        }
-
-        prometheus.scrape "metrics" {
-            scrape_interval = "15s"
-            targets = discovery.relabel.metrics.output
-            forward_to = [prometheus.remote_write.metrics.receiver]
-        }
-
-        prometheus.remote_write "metrics" {
-            endpoint {
-                url = "http://localhost:9090/api/v1/write"
-            }
-        }
-    '';
 
     # services.loki.enable = true;
     # services.loki = {
