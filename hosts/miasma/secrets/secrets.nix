77982a375a6c05b2167c84887c8403bbe62b2a48
donielmaker <daniel.schmidt0204@gmail.com>
2025-11-05 17:30:36 +0100

    rekeyed

--- hosts/miasma/secrets/secrets.nix
+++ hosts/miasma/secrets/secrets.nix
@@ -1,12 +1,12 @@
 let
     donielmaker-zenith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwosE68FthKwXs1WhPnY3YqbkVPT52V30X489epRsJQ donielmaker@zenith";
     donielmaker-galaxia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHTBWBfCU8uM+G5p6vl0dwc0Q7XA6TypesGZWzbbpiwx donielmaker@galaxia";
-    donielmaker-miasma = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFF6799VjHuSA14fxZQ9zI+6FC2yoZTJFW0Cq7ZW3ed5 donielmaker@miasma";
+    donielmaker-miasma = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJkaUMckWwyOjejKH1aYTNXo1hIUnnAtdWKzwome1xH/ donielmaker@miasma";
     users = [ donielmaker-zenith donielmaker-miasma donielmaker-galaxia ];
 
     zenith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE9OsEE+anE42quYmi3ewEsxA/jiopIzdjdAaXa05t/m";
     galaxia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKkebkctZ2f6Df+ujM9XOeDSGteb5QNT8TowV4aEjINK";
-    miasma = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBir991rACOjmulpmKmn7a27R6e8R+OgXFLMRN5aSUeg";
+    miasma = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBdQs+WYWfCL3rjl4Kh4zu/fhI2Tvofx9418qzlTjLOe";
     systems = [ zenith miasma galaxia ];
 in
 {
