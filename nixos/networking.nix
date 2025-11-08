13bb53e70d10f01b7dd07f666987830d8ac08690
donielmaker <daniel.schmidt0204@gmail.com>
2025-11-08 12:36:01 +0100

    added default nameservers

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -1,10 +1,10 @@
-{hostname, ...}:
+{lib, hostname, ...}:
 
 {
     networking.networkmanager.enable = true;
     networking.networkmanager.dns = "none";
     networking.search = [ "thematt.net" ];
-    networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
+    networking.nameservers = lib.mkDefault [ "1.1.1.1" "1.0.0.1" ];
     networking.hostName = hostname;
     networking.useDHCP = false;
     networking.dhcpcd.enable = false;
