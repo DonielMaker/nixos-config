4594c241e1fae97aee3155b9b96710f8a32edc7a
donielmaker <daniel.schmidt0204@gmail.com>
2025-11-16 10:07:54 +0100

    fixed secrets mangement

--- hosts/srv-mx-02/secrets/secrets.nix
+++ hosts/srv-mx-02/secrets/secrets.nix
@@ -1,13 +1,13 @@
 let
     donielmaker-zenith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwosE68FthKwXs1WhPnY3YqbkVPT52V30X489epRsJQ donielmaker@zenith";
     anton-spc-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDwnQCbrIxOgzRpoEYyb/SaoDhM5i93wWJ8biH/f8ygT deuyanon@SPC-01";
-    solut-srv-mx-01 = "";
-    users = [donielmaker-zenith anton-spc-01];
+    solut-srv-mx-02 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXgXDGKQv5n5f+5Zd/elW8mtQJ/6yxqqODx41+08N3o solut@srv-mx-02";
+    users = [donielmaker-zenith anton-spc-01 solut-srv-mx-02];
 
     zenith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE9OsEE+anE42quYmi3ewEsxA/jiopIzdjdAaXa05t/m";
     spc-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFSM5bfdGl9OUuKchV23PVogIf0ZGvH3XeOwtzuuDfep";
-    srv-mx-01 = "";
-    systems = [zenith spc-01];
+    srv-mx-02 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKdVK4zCKA4tjWDd18dPvulsEm7D2kUZvy6XkouBstFs";
+    systems = [zenith spc-01 srv-mx-02];
 
      
 in
