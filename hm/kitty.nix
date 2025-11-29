694bea9e1e13af5f8d8da275d1cd0fba5c928e02
donielmaker <daniel.schmidt0204@gmail.com>
2025-11-29 21:05:55 +0100

    removed unused modules

--- hm/kitty.nix
+++
@@ -1,9 +0,0 @@
-{
-    programs.kitty = {
-        enable = true;
-        shellIntegration.enableZshIntegration = true;
-        themeFile = "Catppuccin-Macchiato";
-        font.name = "FiraCode Nerd Font";
-        font.size = 12;
-    };
-}
