5411a739433840a9e6902a214cb9b0c7fd9ec77c
donielmaker <daniel.schmidt0204@gmail.com>
2025-11-29 11:49:26 +0100

    removed issue

--- nixos/regreet.nix
+++ nixos/regreet.nix
@@ -6,7 +6,7 @@ let
         misc {
             disable_hyprland_logo = true
             disable_splash_rendering = true
-            disable_hyprland_qtutils_check = true
+            # disable_hyprland_qtutils_check = true
         }
     '';
     
