b3f4032a94a4504c08cb49d04a9ab42a34f91317
donielmaker <daniel.schmidt0204@gmail.com>
2025-11-30 01:35:56 +0100

    bunch of improvements to hyprlock

--- hm/hyprlock.nix
+++ hm/hyprlock.nix
@@ -1,10 +1,22 @@
-{lib, ...}: 
+{pkgs, lib, ...}: 
+
+let
+    pfp = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/DonielMaker/wallpapers/9d3e6bb09f9d2745b6224511c7ef3a025666d04a/Matt.jpg";
+        sha256 = "sha256-qIzNEhdXCAfhSZiGqzOdwgcheESpYggVWTj+NfhRQKU=";
+    };
+
+in
 
 {
     programs.hyprlock.enable = true;
     programs.hyprlock.settings = {
         hide_cursor = true;
         ignore_empty_input = true;
+        general = {
+            grace = 0;
+        };
+
         background = {
             path = "screenshot";
             blur_passes = 3;
@@ -14,6 +26,7 @@
             vibrancy = 0.1696;
             vibrancy_darkness = 0.0;
         };
+        
         input-field = {
             outline_thickness = 2;
             fade_on_empty = false;
@@ -22,6 +35,30 @@
             rounding = 20;
             placeholder_text = "no";
             fail_text = "Contacting Administrator";
+            position = "0, -100";
+        };
+
+        image = {
+            path = "${pfp}";
+            border_size = "2";
+            border_color = "rgba(255, 255, 255, 0)";
+            size = "160";
+            rounding = "-1";
+            rotate = "0";
+            reload_time = "-1";
+            position = "0, 100";
+            halign = "center";
+            valign = "center";
+        };
+
+        label = {
+            text = ''cmd[update:1000] echo "<span>$(date +"%I:%M")</span>"'';
+            color = "rgba(216, 222, 233, 0.90)";
+            font_size = 80;
+            font_family = "SF Pro Display Bold";
+            position = "0, -100";
+            halign = "center";
+            valign = "top";
         };
     };
 }
