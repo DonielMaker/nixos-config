7a9d7949e1a87b1d7b950d24f2f7ead4caa01ac7
donielmaker <daniel.schmidt0204@gmail.com>
2025-12-19 19:41:48 +0100

    added hyprlock support to hypridle

--- hm/hypridle.nix
+++ hm/hypridle.nix
@@ -2,6 +2,8 @@
     services.hypridle.enable = true;
     services.hypridle.settings = {
         general = {
+            lock_cmd = "hyprlock";
+            unlock_cmd = "pkill -USR1 hyprlock";
             after_sleep_cmd = "hyprctl dispatch dpms on";
             ignore_dbus_inhibit = false;
         };
@@ -11,6 +13,11 @@
                 timeout = 300;
                 on-timeout = "hyprctl dispatch dpms off";
                 on-resume = "hyprctl dispatch dpms on";
+            }
+
+            {
+                timeout = 310;
+                on-timeout = "hyprlock";
             }
         ];
     };
