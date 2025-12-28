485c2d41ded0d3cf2cf67d8107a6ab8be116541e
donielmaker <daniel.schmidt0204@gmail.com>
2025-12-28 22:33:49 +0100

    formatting

--- hosts/lastprism/secrets/secrets.nix
+++ hosts/lastprism/secrets/secrets.nix
@@ -7,9 +7,12 @@ let
     lastprism = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILFiN6GBFPWtkNhWeSLRTN8yLIXs6BkNQQf1L7nF4Zv3";
     systems = [zenith lastprism];
 in
+
 {
     # Copyparty
     "copyparty/copyparty-donielmaker-password.age".publicKeys = users ++ systems;
+
+    # Outline
     "outline/utilsSecret.age".publicKeys = users ++ systems;
     "outline/secretKey.age".publicKeys = users ++ systems;
     "outline/clientSecret.age".publicKeys = users ++ systems;
