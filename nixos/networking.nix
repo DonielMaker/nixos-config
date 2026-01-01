fc178ea59f4176f94ee6ee66622e1c743a41d4fe
donielmaker <daniel.schmidt0204@gmail.com>
2026-01-01 03:39:56 +0100

    fixed networking

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -1,11 +1,8 @@
-{lib, hostname, ...}:
+{lib, hostname, domain, ...}:
 
 {
     networking.networkmanager.enable = true;
     networking.hostName = hostname;
-    networking.networkmanager.dns = "systemd-resolved";
+    networking.domain = domain;
     networking.nameservers = lib.mkDefault [ "10.10.12.10" "1.1.1.1" ];
-
-    services.resolved.enable = true;
-    services.resolved.domains = lib.mkDefault [ "thematt.net" ];
 }
