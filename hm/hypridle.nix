50b36c052e293a3edc360992f69a1f3ddff4291b
donielmaker <daniel.schmidt0204@gmail.com>
2026-01-25 10:36:48 +0100

    removed suspend from hypridle

--- hm/hypridle.nix
+++ hm/hypridle.nix
@@ -14,15 +14,15 @@
             }
 
             {
-                timeout = 480;
+                timeout = 600;
                 on-timeout = "hyprctl dispatch dpms off";
                 on-resume = "hyprctl dispatch dpms on";
             }
 
-            {
-                timeout = 600;
-                on-timeout = "systemctl suspend";
-            }
+            # {
+            #     timeout = 600;
+            #     on-timeout = "systemctl suspend";
+            # }
         ];
     };
 }
