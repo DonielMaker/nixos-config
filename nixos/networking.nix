e54ab334f61c7f2f2c224018851e0772204f7732
donielmaker <daniel.schmidt0204@gmail.com>
2026-02-01 19:05:22 +0100

    removed domain from networking.nix

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -1,8 +1,7 @@
-{lib, hostname, domain, ...}:
+{lib, hostname, ...}:
 
 {
     networking.networkmanager.enable = true;
     networking.hostName = hostname;
-    networking.domain = domain;
     networking.nameservers = lib.mkDefault [ "10.10.12.10" "1.1.1.1" ];
 }
