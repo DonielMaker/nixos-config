06a16c48a3f4c05151369546a3b5693c3ca12d67
donielmaker <daniel.schmidt0204@gmail.com>
2026-02-24 16:55:20 +0100

    removed screenshots envVar; updated QT_QPA_PLATFORMTHEME to qt6ct

--- hm/hyprland.nix
+++ hm/hyprland.nix
@@ -35,8 +35,7 @@ in
                 "XDG_SESSION_TYPE,wayland"
                 "XDG_SESSION_DESKTOP,Hyprland"
                 "QT_QPA_PLATFORM,wayland"
-                "QT_QPA_PLATFORMTHEME,qt5ct"
-                "XDG_SCREENSHOTS_DIR,~/screenshots"
+                "QT_QPA_PLATFORMTHEME,qt6ct"
             ];
 
             windowrule = [
