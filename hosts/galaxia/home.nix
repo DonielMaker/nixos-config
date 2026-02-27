413d5f97e8a5635b9f5d13a9aa46596950f9ac98
donielmaker <daniel.schmidt0204@gmail.com>
2026-02-27 23:48:37 +0100

    updated galaxia to new config

--- hosts/galaxia/home.nix
+++ hosts/galaxia/home.nix
@@ -1,29 +1,26 @@
-{ inputs, username, ... }:
+{ config, ... }:
 
 {
-    imports = with inputs.self.homeManagerModules; [
-        inputs.stylix.homeModules.stylix
+    modules.hm = {
+        mail = "daniel.schmidt0204@gmail.com";
+        
+        alacritty.enable = true;
+        git.enable = true;
+        neovim.enable = true;
+        starship.enable = true;
+        zellij.enable = true;
+        zsh.enable = true;
 
-        alacritty
-        zellij
-        starship
-        zsh
-        neovim
-        git
-
-        fuzzel
-
-        hyprland
-        hyprlock
-        hypridle
-        quickshell
-        stylix
-    ];
+        stylix.enable = true;
+        librewolf.enable = true;
+        hyprland.enable = true;
+        hyprland.monitor = ", 1920x1080@60hz, auto, 1";
+    };
 
     home = {
-        inherit username;
-        homeDirectory = /home/${username};
-        stateVersion = "25.05";
-        keyboard.layout = "de";
+        inherit (config.modules.hm) username;
+        homeDirectory = "/home/${config.modules.hm.username}";
+        stateVersion = "24.11";
+        keyboard.layout = "us";
     };
 }
