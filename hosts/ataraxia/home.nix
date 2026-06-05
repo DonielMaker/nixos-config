bb70361697cae8592715ce0d13347d8937d84a5a
donielmaker <daniel.schmidt0204@gmail.com>
2026-06-05 16:03:52 +0200

    new host: ataraxia

---
+++ hosts/ataraxia/home.nix
@@ -0,0 +1,30 @@
+{ modules, ... }:
+
+{
+    modules = {
+        desktop = {
+            hyprland.enable = true;
+            noctalia.enable = true;
+            stylix.enable = true;
+        };
+
+        programs = {
+            obsidian.enable = true;
+        };
+
+        terminal = {
+            alacritty.enable = true;
+            git.enable = true;
+            neovim.enable = true;
+            starship.enable = true;
+            zellij.enable = true;
+            zsh.enable = true;
+        };
+    };
+
+    home = {
+        inherit (modules.system) username;
+        homeDirectory = "/home/${modules.system.username}";
+        stateVersion = "25.11";
+    };
+}
