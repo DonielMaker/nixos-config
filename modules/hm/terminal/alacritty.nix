58bd12b764d42f845708184f4513e67ac1461ef3
donielmaker <daniel.schmidt0204@gmail.com>
2026-07-02 14:57:23 +0200

    removed decoration option from alacritty

--- modules/hm/terminal/alacritty.nix
+++ modules/hm/terminal/alacritty.nix
@@ -11,7 +11,6 @@ in
         programs.alacritty.enable = true;
         programs.alacritty.settings = {
             window = {
-                decorations = "None";
                 padding = { x = 10; y = 10; };
             };
         };
