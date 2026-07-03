120ea1ac018bdeb23e1da8f9e04c00968886010f
donielmaker <daniel.schmidt0204@gmail.com>
2026-07-03 19:05:00 +0200

    moved workspace 4 to main monitor

--- hosts/zenith/home.nix
+++ hosts/zenith/home.nix
@@ -1,14 +1,13 @@
 { osConfig, ... }:
 
 {
-
     services.easyeffects.enable = true;
 
     wayland.windowManager.hyprland.settings.workspace = [
         "1, monitor:DP-1, default:true"
         "2, monitor:DP-1"
         "3, monitor:DP-1"
-        "4, monitor:DP-2"
+        "4, monitor:DP-1"
 
         "5, monitor:DP-2, default:true, layout:scrolling, layoutopt:direction:down"
         "6, monitor:DP-2, layout:scrolling, layoutopt:direction:down"
